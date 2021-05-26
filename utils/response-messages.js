2d204b291718b321161a35b582fdaf0862db812b
Damian <wojtunikdw@gmail.com>
2021-05-26 19:55:54 +0100

    Created custom response messages for HTTP res

---
+++ utils/response-messages.js
@@ -0,0 +1,9 @@
+exports.WRONG_FORM_STRUCTURE = "There are unexpected or missing fields in the form you have submitted. Please try again or contact the support.";
+
+exports.MISSING_STORY_FIELDS = "Please make sure that you fill in all the fields in each user story and try again.";
+
+exports.WRONG_CAPTCHA = "Please make sure that you pass the Captcha verification before proceeding.";
+
+exports.REQUEST_ACCEPTED = "Your request has been accepted. Once your user stories have been analysed, you will receive your UML diagrams via e-mail.";
+
+exports.REQUEST_FAILURE = "There has been a problem while processing your request. Please try again or contact the support.";
