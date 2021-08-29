196e6f26c53dccad49d04ecbdf1e7eb6991afc44
Damian <wojtunikdw@gmail.com>
2021-08-29 16:15:20 +0100

    HTTP change

--- utils/emails/mailingService.js
+++ utils/emails/mailingService.js
@@ -4,7 +4,7 @@ sgMail.setApiKey(process.env.SENDGRID_KEY);
 exports.sendEmailForCompletedAnalysis = (data, email) => {
     
     if(!!data.ucParam && !!data.classParam) {
-        const emailMessage = `Dear user, <br /><br />ReqUML has completed the analysis of your user stories. Follow the URL below to preview and download your use case diagrams. <br /><br /><a href="https://ready.requml.co.uk/uc/${data.ucParam}"><b>https://ready.requml.co.uk/uc/${data.ucParam}</b></a><br /><br /><a href="https://ready.requml.co.uk/class/${data.classParam}"><b>https://ready.requml.co.uk/class/${data.classParam}</b></a><br /><br />Should you have any questions, do not hesitate to contact us at <b>contact@requml.co.uk</b><br /><br />Best wishes,<br />ReqUML Team`;
+        const emailMessage = `Dear user, <br /><br />ReqUML has completed the analysis of your user stories. Follow the URL below to preview and download your use case diagrams. <br /><br /><a href="http://ready.requml.co.uk/uc/${data.ucParam}"><b>http://ready.requml.co.uk/uc/${data.ucParam}</b></a><br /><br /><a href="http://ready.requml.co.uk/class/${data.classParam}"><b>http://ready.requml.co.uk/class/${data.classParam}</b></a><br /><br />Should you have any questions, do not hesitate to contact us at <b>contact@requml.co.uk</b><br /><br />Best wishes,<br />ReqUML Team`;
 
         const msg = {
             to: email,
